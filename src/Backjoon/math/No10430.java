a111116e0abb46b2ca28a7e5da9fdb60b06164ef
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2023-02-13 17:17:02 +0900

    수학 문제 추가

---
+++ src/Backjoon/math/No10430.java
@@ -0,0 +1,25 @@
+package Backjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No10430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        sb.append((A + B) % C).append("\n");
+        sb.append(((A % C) + (B % C)) % C).append("\n");
+        sb.append((A * B) % C).append("\n");
+        sb.append(((A % C) * (B % C)) % C).append("\n");
+
+        System.out.println(sb);
+    }
+}

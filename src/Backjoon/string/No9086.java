94b63becda32eebe7624a94c266a32248692ee46
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2023-03-03 00:03:53 +0900

    문자열 문제 추가

---
+++ src/Backjoon/string/No9086.java
@@ -0,0 +1,21 @@
+package Backjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No9086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            sb.append(s.charAt(0));
+            sb.append(s.charAt(s.length() - 1)).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}

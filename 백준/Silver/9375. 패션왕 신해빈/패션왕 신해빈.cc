b5dde08514ebf929bc0e3855a049a28991907f3b
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2023-08-10 23:01:25 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
-int T, N, ret;
-map<string, int> mp;
+int T, N;
 string value, key;
 
 int main() {
@@ -10,21 +9,19 @@ int main() {
 	cin >> T;
 	
 	while(T--){
-		ret = 1;
+		map<string, int> _map;
 		cin >> N;
 		for(int i = 0; i < N; i++){
 			cin >> value >> key;
-			if(mp[key] == 0) mp[key] = 1;
-			else mp[key] += 1;
+			_map[key]++;
 		}
 		
-		for(auto it : mp) {
-			ret *= (it.second + 1);
+		long long ret = 1;
+		for(auto it : _map) {
+			ret *= ((long long)it.second + 1);
 		}
 		
 		cout << ret - 1 << "\n";
-		
-		mp.clear();
 	}
 	
 	return 0; 

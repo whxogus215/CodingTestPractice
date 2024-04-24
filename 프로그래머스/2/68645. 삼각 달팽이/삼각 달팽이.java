8d6f595deaa36a17705036e5b0c717d57eccd914
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-04-24 23:09:29 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.58 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,44 +1,27 @@
 class Solution {
 
-    /**
-     * 1. 아래로 배열 채우기 -> 오른쪽으로 배열 채우기 -> 왼쪽 위 대각선으로 배열 채우기 (반복) 2. (0,0)에서 시작 3. while(true)로 반복
-     * 2. 이차원 배열을 일차원 배열로 변환
-     */
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+    
     public int[] solution(int n) {
         int[][] triangle = new int[n][n];
         int number = 1;
         int x = 0;
         int y = 0;
+        int d = 0;
 
         while (true) {
-            // 아래로 이동
-            while (true) {
-                triangle[y][x] = number++;
-                if (y + 1 == n || triangle[y + 1][x] != 0) break;
-                y += 1; // 아래로 이동
+            triangle[y][x] = number++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
             }
-            if (x + 1 == n || triangle[y][x + 1] != 0) break;
-            x += 1;
-
-            // 오른쪽으로 이동
-            while (true) {
-                triangle[y][x] = number++;
-                if (x + 1 == n || triangle[y][x + 1] != 0) break;
-                x += 1;
-            }
-            if (triangle[y - 1][x - 1] != 0) break;
-            x -= 1;
-            y -= 1;
-
-            // 왼쪽 위대각선으로 이동
-            while (true) {
-                triangle[y][x] = number++;
-                if (triangle[y - 1][x - 1] != 0) break;
-                x -= 1;
-                y -= 1;
-            }
-            if (y + 1 == n || triangle[y + 1][x] != 0) break;
-            y += 1;
+            x = nx;
+            y = ny;
         }
 
         int[] result = new int[number - 1];
@@ -48,7 +31,7 @@ class Solution {
                 result[index++] = triangle[i][j];
             }
         }
-        
+
         return result;
     }
 

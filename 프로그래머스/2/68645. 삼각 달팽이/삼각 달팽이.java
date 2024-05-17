a519bb0972de590d7051ee9405bb9ec84d00a44e
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-05-17 11:24:16 +0900

    [level 2] Title: 삼각 달팽이, Time: 10.88 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,38 +1,53 @@
 class Solution {
 
-    private static final int[] dx = {0, 1, -1};
-    private static final int[] dy = {1, 0, -1};
-    
     public int[] solution(int n) {
-        int[][] triangle = new int[n][n];
-        int number = 1;
-        int x = 0;
+        int[][] matrix = new int[n][n];
+        int value = 1;
         int y = 0;
-        int d = 0;
+        int x = 0;
+        
 
         while (true) {
-            triangle[y][x] = number++;
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+            // 아래로 내려가기
+            while (true) {
+                matrix[y][x] = value++;
+                if (y + 1 >= n || matrix[y + 1][x] != 0) break;
+                y += 1;
             }
-            x = nx;
-            y = ny;
+            // 아래로 채우는게 마지막일 경우, while문을 다시 빠져나와야 함
+            if (x + 1 >= n || matrix[y][x + 1] != 0) break;
+            // 오른쪽으로 가기
+            x += 1;
+            while (true) {
+                matrix[y][x] = value++;
+                if (x + 1 >= n || matrix[y][x + 1] != 0) break;
+                x += 1;
+            }
+            // 오른쪽으로 채우는게 마지막일 경우, while문을 다시 빠져나와야 함
+            if (matrix[y - 1][x - 1] != 0) break;
+            // 왼쪽 대각선으로 가기
+            y -= 1;
+            x -= 1;
+            while (true) {
+                matrix[y][x] = value++;
+                if (matrix[y - 1][x - 1] != 0) break;
+                y -= 1;
+                x -= 1;
+            }
+            // 왼쪽 대각선으로 채우는게 마지막일 경우, while문을 다시 빠져나와야 함
+            if (y + 1 >= n || matrix[y + 1][x] != 0) break;
+            y += 1;
         }
 
-        int[] result = new int[number - 1];
+        int[] answer = new int[value - 1];
         int index = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                result[index++] = triangle[i][j];
+                answer[index++] = matrix[i][j];
             }
         }
 
-        return result;
+        return answer;
     }
 
 }

82d51523bf9723085435000751b0322001c747d8
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-05-22 10:45:40 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,79 +1,71 @@
 class Solution {
-
-    private static final int dy[] = {-1, 0, 0, 1};
-    private static final int dx[] = {0, -1, 1, 0};
-
     /**
-     * 1. 대기실의 모든 좌표를 탐색한다.
-     * 2. 상하좌우로 탐색 시, 빈 테이블이 있을 경우, 해당 위치로 이동하여 주변을 탐색한다.
-     * 3. 이 때, 응시자가 발견될 경우, 거리두기를 지키지 않은 것으로 처리한다.
-     *
-     * ** String[]를 char[][]로 변환한다!!!!
-     *
-     * *** 메서드 구현순서
-     * 대기실이 여러 개 -> 각 대기실마다 로직을 적용 -> 각 대기실을 이차원 배열로 변환 -> 변환한 이차원 배열을 매개변수로 하여 검사
-     * -> 응시자가 있는 좌표에 대해 거리두기 검사(메서드 오버로딩)
+     * 1. 한 대기실에 대한 검사 로직을 5번 반복한다.
+     * 2. 한 대기실은 String[]로 제공되며, 상하좌우 좌표로 접근하기 위해 이차원 배열로 변경한다. O
+     * 3. P인 지점을 기준으로 상하좌우 검사를 통해 X, O, P인지에 따라 다른 로직을 수행한다.
+     * 3-1. P 옆에 X가 있을 경우, 해당 방향은 거리두기를 모두 준수하므로 Pass
+     * 3-2. P 옆에 O가 있을 경우, O인 지점을 기준으로 세 방향 탐색 했을 때 P가 나오면 False
+     * 3-3. P 옆에 P가 있을 경우, 바로 False
      */
+    private final int[] dy = {-1, 0, 0, 1};
+    private final int[] dx = {0, -1, 1, 0};
 
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
-        for (int i = 0; i < answer.length; i++) {
+        int answer[] = new int[places.length];        
+        
+        for (int i = 0; i < places.length; i++) { // 5번 로직이 반복된다.
             String[] place = places[i];
-            // String[]을 char[][]로 변환한다.
             char[][] room = new char[place.length][];
-            for (int j = 0; j < room.length; j++) {
-                room[j] = place[j].toCharArray();
+            for (int j = 0; j < place.length; j++) {
+                room[j] = place[j].toCharArray(); // 이차원 문자형 배열로 변환
             }
-            // 변환된 char[][]의 좌표를 하나하나 탐색하며 거리두기를 검사한다.
-            if (isDistanced(room)) {
-                answer[i] = 1;
-            } else {
+            
+            if (!isDistanced(room)) { // 거리두기 준수하지 않을 시, 0 저장
                 answer[i] = 0;
+            } else {
+                answer[i] = 1;
             }
         }
         return answer;
     }
-
+    
     private boolean isDistanced(char[][] room) {
+        // 해당 룸의 모든 좌표를 탐색한다.
         for (int y = 0; y < room.length; y++) {
             for (int x = 0; x < room[y].length; x++) {
-                if (room[y][x] != 'P') continue; // 응시자가 없을 경우 Pass!
-                // P인 지점의 좌표를 전달하여 거리두기를 확인한다.
-                if (!isDistanced(room,x,y)) return false; // 거리두기 결과 값이 false일 경우, 거리두기 지키지 않은 것으로 간주!
+                if (room[y][x] == 'P') { // P인 지점에 대해서만 검사
+                    if (!isDistanced(room, y, x)) return false;
+                }
             }
         }
-        // 모든 탐색을 마칠 경우, 거리두기를 지킨 것으로 간주
         return true;
     }
-
-    private boolean isDistanced(char[][] room, int x, int y) { // room[y][x]에 대한 거리두기 검사
-        // P인 지점에 대한 검사이므로 4 방향에 대해 탐색하기
+    
+    private boolean isDistanced(char[][] room, int y, int x) {
         for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
             int ny = y + dy[d];
+            int nx = x + dx[d];
+
             if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
-            switch (room[ny][nx]) {
-                case 'P': return false; // P 옆에 P가 있으면 false;
-                case 'O': // O인 지점에서 상하좌우 P 있는지 검사
-                    if (isNextToVolunteer(room, nx, ny, 3 - d)) return false;
-                    break;
+
+            if (room[ny][nx] == 'P') return false;
+            if (room[ny][nx] == 'O') { // O인 지점을 기준으로 세방향 탐색
+                if (isNextToPerson(room, ny, nx, 3 - d)) return false; // O 옆에 P가 또 있는 것이므로 false 반환
             }
         }
-        // 모든 탐색을 마칠 경우, 거리두기를 지킨 것으로 간주
         return true;
     }
-
-    private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude) {
-        // 검사를 한 방향은 제외하고 검사해야 함
+    
+    private boolean isNextToPerson(char[][] room, int y, int x, int exclude) {
         for (int d = 0; d < 4; d++) {
             if (d == exclude) continue;
-            
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
-            if (room[ny][nx] == 'P') return true;
-        }
-        return false; // 주변에 P가 없다는 뜻
-    }
 
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+
+            if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
+            if (room[ny][nx] == 'P') return true; // O 지점 옆에 P가 있음
+        }
+        return false;
+    }
 }

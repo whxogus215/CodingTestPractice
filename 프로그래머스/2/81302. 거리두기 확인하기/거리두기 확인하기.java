d0fb56162702512df5f98375a9856dbdc4aa9c03
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-05-22 10:49:43 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.06 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -11,15 +11,15 @@ class Solution {
     private final int[] dx = {0, -1, 1, 0};
 
     public int[] solution(String[][] places) {
-        int answer[] = new int[places.length];        
-        
+        int answer[] = new int[places.length];
+
         for (int i = 0; i < places.length; i++) { // 5번 로직이 반복된다.
             String[] place = places[i];
             char[][] room = new char[place.length][];
             for (int j = 0; j < place.length; j++) {
                 room[j] = place[j].toCharArray(); // 이차원 문자형 배열로 변환
             }
-            
+
             if (!isDistanced(room)) { // 거리두기 준수하지 않을 시, 0 저장
                 answer[i] = 0;
             } else {
@@ -28,19 +28,21 @@ class Solution {
         }
         return answer;
     }
-    
+
     private boolean isDistanced(char[][] room) {
         // 해당 룸의 모든 좌표를 탐색한다.
         for (int y = 0; y < room.length; y++) {
             for (int x = 0; x < room[y].length; x++) {
-                if (room[y][x] == 'P') { // P인 지점에 대해서만 검사
-                    if (!isDistanced(room, y, x)) return false;
-                }
+//                if (room[y][x] == 'P') { // P인 지점에 대해서만 검사
+//                    if (!isDistanced(room, y, x)) return false;
+//                }
+                if (room[y][x] != 'P') continue;
+                if (!isDistanced(room, y, x)) return false;
             }
         }
         return true;
     }
-    
+
     private boolean isDistanced(char[][] room, int y, int x) {
         for (int d = 0; d < 4; d++) {
             int ny = y + dy[d];
@@ -55,7 +57,7 @@ class Solution {
         }
         return true;
     }
-    
+
     private boolean isNextToPerson(char[][] room, int y, int x, int exclude) {
         for (int d = 0; d < 4; d++) {
             if (d == exclude) continue;

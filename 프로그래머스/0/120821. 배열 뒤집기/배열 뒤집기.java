30111ddc32b4551f4247fc60a0f5f551a8b6103d
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-06-08 11:40:56 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.43 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,9 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
-        for (int i = 0; i < num_list.length; i++) {
-            answer[i] = num_list[num_list.length - 1 - i];
-        }
-        return answer;
+        final List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
+        Collections.reverse(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }

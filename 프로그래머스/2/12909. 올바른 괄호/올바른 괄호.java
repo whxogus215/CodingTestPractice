51b21e511b93c88f40d82758dd8ceebd0d0c0af5
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-07-25 17:04:51 +0900

    [level 2] Title: 올바른 괄호, Time: 15.32 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,29 @@
+import java.util.ArrayDeque;
 import java.util.Stack;
 
 /**
  * 1. (가 나오면 일단 스택에 저장한다.
  * 2. )가 나오면 스택에서 (를 꺼낸다.
  * 3. 만약, 꺼낼 (가 없을 경우, 이는 잘못된 괄호이다.
+ *
+ * 스택으로 접근해야 하는 이유 : 짝이 맞는 괄호는 서로 상쇄된다.
+ * ==> 가장 최근에 조회한 열린괄호가 먼저 상쇄된다 -> LIFO(FILO) -> 스택!!!
  */
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(') {
-                stack.push(ch);
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        char[] a = s.toCharArray();
+        for (char c : a) {
+            if (c == '(') {
+                stack.push(c);
             } else {
-                if (stack.isEmpty()) {
+                if (stack.isEmpty() || stack.pop() == c) {
                     return false;
                 }
-                stack.pop();
             }
         }
-        if (!stack.isEmpty()) return false;
-        return true;
+
+        return stack.isEmpty();
     }
 }
 

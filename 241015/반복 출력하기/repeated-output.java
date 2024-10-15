a9857d3dbe16055d464b214ba0bc42527f5b5752
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-15 21:20:54 +0900

    Title: 반복 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241015/반복 출력하기/repeated-output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        print(N);
+    }
+
+    public static void print(int N) {
+        for (int i = 0; i < N; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}

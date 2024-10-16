1d5e1021baf6d1066e3979c8f47d70073ee4fab6
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-16 16:37:06 +0900

    Title: 최소공배수 구하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/최소공배수 구하기/find-the-least-common-multiple.java
+++ 241016/최소공배수 구하기/find-the-least-common-multiple.java
@@ -10,17 +10,13 @@ public class Main {
     }
 
     public static void printMinCommon(int n, int m) {
-        int min = Math.min(n, m);
-        int max = Math.max(n, m);
-        int minIndex = 1, maxIndex = 1;
-
-        while(min != max) {
-            if (min < max) {
-                min = Math.min(n, m) * (++minIndex);
-            } else if (min > max) {
-                max = Math.max(n, m) * (++maxIndex);
+        int gcd = 0;
+        for(int i = 1; i <= Math.min(n, m); i++) {
+            if (n % i == 0 && m % i == 0) {
+                gcd = i;
             }
         }
-        System.out.println(min);
+
+        System.out.println(n * m / gcd);
     }
 }

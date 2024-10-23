4dc75ec70d68868ecaf910b344008964e9ef01b8
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-23 14:33:14 +0900

    Title: 2개 이상의 알파벳, Time: 128ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241023/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        if (isDifferent(input)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isDifferent(String input) {
+        char[] arr = new char[input.length()];
+
+        char value = ' ';
+        int index = 0;
+        arr[0] = input.charAt(0);
+        A:for(int i = 1; i < input.length(); i++) {
+            value = input.charAt(i);
+            for(int j = 0; j < index + 1; j++) {
+                if (arr[j] == value) {
+                    break A;
+                }
+            }
+            arr[++index] = value;
+        }
+
+        if (arr.length >= 2) {
+            return true;
+        }
+        return false;
+    }
+}

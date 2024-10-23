31f74bc6641d7c65ef4382b48c1dedf97e35da04
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-23 15:10:04 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 111ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241023/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static String input;
+    public static String object;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        input = sc.next();
+        object = sc.next();
+
+        System.out.println(isContain());
+    }
+
+    public static int isContain() {
+        for(int i = 0; i <= input.length() - 2; i++) {
+            if (object.equals(input.substring(i, i + object.length()))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

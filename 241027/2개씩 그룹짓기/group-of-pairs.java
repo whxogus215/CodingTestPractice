9d18bb14f8bf46da29fa607ce05f4dd3364dad97
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-27 10:28:23 +0900

    Title: 2개씩 그룹짓기, Time: 205ms, Memory: 13MB, Status: Passed - CodeTree

--- 241027/2개씩 그룹짓기/group-of-pairs.java
+++ 241027/2개씩 그룹짓기/group-of-pairs.java
@@ -1,31 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    
     public static final int MAX_N = 1000;
-    public static int N;
-    public static int[] arr = new int[MAX_N];
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[] nums = new int[2 * MAX_N];
+
+        for(int i = 0; i < 2 * n; i++)
+            nums[i] = sc.nextInt();
+
+        // nums[0]부터 nums[2n - 1]까지를 정렬함
+        Arrays.sort(nums, 0, 2 * n);
+
+        int groupMax = 0;
+        for(int i = 0; i < n; i++) {
+            // i번째와 2n - 1 - i번째 원소를 매칭
+            int groupSum = nums[i] + nums[2*n - 1 - i];
+            if(groupSum > groupMax)
+                // 최댓값 갱신
+                groupMax = groupSum;
         }
 
-        Arrays.sort(arr, 0, N);
+        System.out.print(groupMax);
 
-        System.out.println(groupMax());
-    }
-
-    public static int groupMax() {
-        int max = -1;
-
-        for(int i = 0; i < N - 1; i++) {
-            int sum = arr[i] + arr[i + 1];
-            max = Math.max(sum, max);
-        }
-
-        return max;
     }
 }

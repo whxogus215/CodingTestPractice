aa6faf63ffcb692a6b9760669f2079b64596ea93
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-10-29 13:09:53 +0900

    Title: 사는 지역, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/사는 지역/where-live.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Person[] people = new Person[n];
+
+        for(int i = 0; i < n; i++) {
+            String name = sc.next();
+            String bungi = sc.next();
+            String city = sc.next();
+            people[i] = new Person(name, bungi, city);
+        }
+
+        String[] names = new String[n];
+        for(int i = 0; i < n; i++) {
+            names[i] = people[i].name;
+        }
+
+        Arrays.sort(names);
+
+        System.out.println("name " + people[n - 1].name);
+        System.out.println("addr " + people[n - 1].bungi);
+        System.out.println("city " + people[n - 1].city);
+    }
+}
+
+class Person {
+    String name;
+    String bungi;
+    String city;
+
+    public Person(String name, String bungi, String city) {
+        this.name = name;
+        this.bungi = bungi;
+        this.city = city;
+    }
+}

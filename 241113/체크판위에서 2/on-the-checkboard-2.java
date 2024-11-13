e9ce4b60f829ce31654f9797edd12d8a8e358a19
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-11-13 17:11:09 +0900

    Title: 체크판위에서 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241113/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+    private static char[][] map = new char[15][15];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                map[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 1; i < R - 2; i++) {
+            char before = map[0][0];
+            for(int j = 1; j < C - 2; j++) {
+                if (map[i][j] != before) {
+                    before = map[i][j];
+                    for(int k = i + 1; k < R - 1; k++) {
+                        for(int l = j + 1; l < C - 1; l++) {
+                            if (map[k][l] != before && map[k][l] != map[R - 1][C - 1]) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}

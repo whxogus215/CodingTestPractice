2f21a22116d3bd7ba214fc9365186f5e512916c1
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-11-17 08:58:42 +0900

    Title: 방향에 맞춰 이동, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241117/방향에 맞춰 이동/move-in-direction.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, -1, 0, 1};
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("E", 0);
+        map.put("S", 1);
+        map.put("W", 2);
+        map.put("N", 3);
+        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int x = 0;
+        int y = 0;
+
+        for(int i = 0; i < N; i++) {
+            String dir = sc.next();
+            int distance = sc.nextInt();
+
+            int index = map.get(dir);
+
+            x += dx[index] * distance;
+            y += dy[index] * distance;
+        }
+
+        System.out.println(x + " " + y);
+    }
+}

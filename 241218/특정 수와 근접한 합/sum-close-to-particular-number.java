68cbb56273f5479338676df4b0ac7f15eb170c10
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-12-18 11:39:06 +0900

    Title: 특정 수와 근접한 합, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241218/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    private static int[] numbers = new int[100];
+    private static int N;
+    private static int S;
+    private static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int S = sc.nextInt();
+
+        for(int i = 0; i < N; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = i + 1; j < N; j++) {
+                min = Math.min(min, Math.abs(S - getSumIgnore(numbers[i], numbers[j], N)));
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    private static int getSumIgnore(int first, int second, int N) {
+        int sum = 0;
+        for(int i = 0; i < N; i++) {
+            if (numbers[i] == first || numbers[i] == second) {
+                continue;
+            }
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+}

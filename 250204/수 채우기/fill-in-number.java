fc48a1c5425b8079430ddf76e67be7eb618a1d79
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-02-04 21:20:03 +0900

    Title: 수 채우기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250204/수 채우기/fill-in-number.java
+++ 250204/수 채우기/fill-in-number.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         int result = 100_000;
 
-        for (int i = 0; i <= 2000; i++) {
+        for (int i = 0; i <= 20000; i++) {
             int remainder = n - (5 * i);
             if (remainder >= 0 && remainder % 2 == 0) {
                 result = Math.min(result, i + (remainder / 2));

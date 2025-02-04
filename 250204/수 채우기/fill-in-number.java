4237193802c74be35c671170dea461e1bea8e3c5
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-02-04 21:16:28 +0900

    Title: 수 채우기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250204/수 채우기/fill-in-number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 100_000;
+
+        for (int i = 0; i <= 2000; i++) {
+            int remainder = n - (5 * i);
+            if (remainder >= 2 && remainder % 2 == 0) {
+                result = Math.min(result, i + (remainder / 2));
+            }
+        }
+
+        if (result == 100_000) {
+            result = -1;
+        }
+
+        System.out.println(result);
+    }
+}

2c72e215efc78f1c6700cd0c61e688d2366eb620
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-03-06 12:58:32 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 250306/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.java
+++ 250306/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.java
@@ -1,22 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static int a, b, c;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
 
-        int max = 0;
-        for(int i = 0; i * A <= C; i++) {
-            for(int j = 0; j * B <= C; j++) {
-                int sum = A * i + B * j; 
-                if (sum > C) {
-                    break ;
-                }
-                max = Math.max(max, sum);
-            }
+        int ans = 0;
+
+        // a를 몇 회 사용할지 전부 시도해 봅니다.
+        // 모든 경우의 수에 대해 최대가 되도록 하는 수를 계산합니다.
+        for(int i = 0; i * a <= c; i++) {
+            // a를 i회 사용합니다.
+            int cnt = a * i;
+
+            // 값을 최대로 하기 위해 b를 몇회 사용해야 하는지 계산합니다.
+            int numB = (c - cnt) / b;
+
+            cnt += numB * b;
+            
+            ans = Math.max(ans, cnt);
         }
-        System.out.println(max);
+
+        System.out.print(ans);
     }
 }

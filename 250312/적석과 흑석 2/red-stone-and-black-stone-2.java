e98e03c7166f008ca4842b45e7d983b3ff634649
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-03-12 22:06:42 +0900

    Title: 적석과 흑석 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250312/적석과 흑석 2/red-stone-and-black-stone-2.java
+++ 250312/적석과 흑석 2/red-stone-and-black-stone-2.java
@@ -1,70 +1,60 @@
-
-
-// 클로드랑 대화하면서 해설에서 제시한 알고리즘 답안에 대해 이해하기!!
-// 우선순위 큐 방식으로는 할 수 없는 것인가? => 
-
-
-
 import java.util.*;
 
 public class Main {
-    
-    public static int C, N;
-    
+
+    public static TreeSet<Integer> tree = new TreeSet<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        C = sc.nextInt();
-        N = sc.nextInt();
-        
-        PriorityQueue<Integer> redStones = new PriorityQueue<>(); // 최소 힙
-        PriorityQueue<BlackStone> blackStones = new PriorityQueue<>(
-            // B를 기준으로 오름차순, B가 같으면 A를 기준으로 내림차순
-            (b1, b2) -> {
-                if (b1.B == b2.B) {
-                    return b1.A - b2.A;
-                }
-                return b1.B - b2.B;
-            }
-        );
-
-        for(int i = 0; i < C; i++) {
-            int redStone = sc.nextInt();
-            redStones.add(redStone);
-        }
+        int N = sc.nextInt();
 
         for(int i = 0; i < N; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            blackStones.add(new BlackStone(A, B));
-        }
-
-        int maxPair = 0;
-        for(int i = 0; i < C; i++) {
-            if (redStones.isEmpty() || blackStones.isEmpty()) {
-                break;
-            }
-            int redStone = redStones.peek();
-            BlackStone blackStone = blackStones.peek();
-            if (redStone < blackStone.A) {
-                redStones.poll();
-            } else if (redStone > blackStone.B) {
-                blackStones.poll();
+            String line = sc.nextLine();
+            String[] commands = line.split(" ");
+            System.out.println(Arrays.toString(commands));
+            if (commands.length == 2) {
+                go(commands[0], Integer.parseInt(commands[1]));
             } else {
-                redStones.poll();
-                blackStones.poll();
-                maxPair++;
+                go(line, 0);
             }
         }
-        System.out.println(maxPair);
     }
 
-    static class BlackStone {
-        int A;
-        int B;
-
-        public BlackStone(int A, int B) {
-            this.A = A;
-            this.B = B;
+    public static void go(String command, int number) {
+        if ("add".equals(command)) {
+            tree.add(number);
+        } else if ("remove".equals(command)) {
+            tree.remove(number);
+        } else if ("find".equals(command)) {
+            if (tree.contains(number)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        } else if ("lower_bound".equals(command)) {
+            if (tree.ceiling(number) != null) {
+                System.out.println(tree.ceiling(number));
+            } else {
+                System.out.println("None");
+            }
+        } else if ("upper_bound".equals(command)) {
+            if (tree.higher(number) != null) {
+                System.out.println(tree.higher(number));
+            } else {
+                System.out.println("None");
+            }
+        } else if ("largest".equals(command)) {
+            if (!tree.isEmpty()) {
+                System.out.println(tree.last());
+            } else {
+                System.out.println("None");
+            }
+        } else {
+            if (!tree.isEmpty()) {
+                System.out.println(tree.first());
+            } else {
+                System.out.println("None");
+            }
         }
     }
 }

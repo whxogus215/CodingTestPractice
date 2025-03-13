de54df818ac4a302f3d846197f304dd5b4712b80
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-03-13 16:19:10 +0900

    Title: 훌륭한 점프, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250313/훌륭한 점프/great-jump.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    public static int MAX_N = 100;
+    public static int N, K;
+    public static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = MAX_N;
+        for(int max = MAX_N; max >= 1; max--) {
+            if(isPossible(max)) {
+                result = Math.min(result, max);
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static boolean isPossible(int max) {
+        int[] available = new int[N];
+        int index = 1;
+        for(int i = 1 ; i < N; i++) {
+            if (arr[i] <= max) {
+                available[index++] = i; 
+            }
+        }
+
+        for(int i = 1; i < index; i++) {
+            if (available[i] - available[i - 1] > K) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

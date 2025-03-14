946e5ce359e7e58a170235ffcb146e968ebd9d4b
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-03-14 15:22:14 +0900

    Title: 초기 수열 복원하기, Time: 122ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250314/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int[] sum = new int[N - 1];
+
+        for(int i = 0; i < N - 1; i++) {
+            sum[i] = sc.nextInt();
+        }
+
+        for(int i = 1; i <= N; i++) {
+            int[] arr = new int[N];
+            int[] isExist = new int[N + 1];
+            
+            arr[0] = i;
+            isExist[i] = 1;
+
+            int startIndex = 1;
+            boolean isComplete = true;
+            for(int j = 1; j <= N; j++) {
+                if (j == i) {
+                    continue;
+                }
+                arr[startIndex] = sum[startIndex - 1] - arr[startIndex - 1];
+                if (isExist[arr[startIndex]] == 1) {
+                    isComplete = false;
+                    break;
+                } else {
+                    isExist[arr[startIndex]] = 1;
+                    startIndex++;
+                }
+            }
+            if (isComplete) {
+                for(int k = 0; k < N; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                return;
+            }
+        }
+
+    }
+}

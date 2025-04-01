c71193cecfe4b877bec3d544dbe19fa6e4aa9acb
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-04-01 22:03:55 +0900

    Title: 계단 오르기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250401/계단 오르기/climbing-stairs.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int DIVIDE = 10007;
+    public static int[] memo;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+
+        int result = step(n);
+        System.out.println(result % DIVIDE);
+    }
+
+    public static int step(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+
+        if (n == 0) {
+            memo[n] = 1;
+        } else {
+            memo[n] = step(n - 3) + step(n - 2);
+        }
+
+        return memo[n];
+    }
+}

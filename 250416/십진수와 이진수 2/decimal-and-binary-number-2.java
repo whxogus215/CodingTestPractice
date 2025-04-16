24b82fd33d8cafa319792b5ff3d5ba15d2f7590e
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-04-16 13:04:38 +0900

    Title: 십진수와 이진수 2, Time: 116ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250416/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+        
+
+        int result = convertTen(binary);
+        System.out.println(convertTwo(result * 17));
+    }
+
+    public static int convertTen(String binary) {
+        int sum = 0;
+        for(int i = 0; i < binary.length(); i++) {
+            sum = sum * 2 + (binary.charAt(i) - '0');
+        }
+        return sum;
+    }
+
+    public static String convertTwo(int digit) {
+        int[] binary = new int[10];
+        int cnt = 0;
+        
+        while(true) {
+            if (digit < 2) {
+                binary[cnt] = digit % 2;
+                break;
+            }
+
+            binary[cnt++] = digit % 2;
+            digit /= 2;
+        }
+
+        String result = "";
+        for(int i = cnt; i >= 0; i--) {
+            result += binary[i];
+        }
+        return result;
+    }
+}

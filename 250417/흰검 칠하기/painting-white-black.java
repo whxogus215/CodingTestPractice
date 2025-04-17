9316d672a31ec3648f6c4bebf0ec875caf3c1007
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-04-17 12:48:28 +0900

    Title: 흰검 칠하기, Time: 238ms, Memory: 16MB, Status: Passed - Codetree

--- 250417/흰검 칠하기/painting-white-black.java
+++ 250417/흰검 칠하기/painting-white-black.java
@@ -11,50 +11,39 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int cPos = OFFSET;
-        int nPos;
         for (int j = 0; j < N; j++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
 
             if (d == 'L') {
-                nPos = cPos - (x - 1);
-                for(int i = nPos; i <= cPos; i++) {
-                    if (tiles[i] == 'G') {
-                        continue;
-                    }
-                    white[i]++;
-                    if (isGrey(i)) {
-                        tiles[i] = 'G';
-                    } else {
-                        tiles[i] = 'W';
+                while(x-- > 0) {
+                    tiles[cPos] = 'W';
+                    white[cPos]++;
+                    if (x > 0) {
+                        cPos--;
                     }
                 }
             } else {
-                nPos = cPos + (x - 1);
-                for(int i = cPos; i <= nPos; i++) {
-                    if (tiles[i] == 'G') {
-                        continue;
-                    }
-                    black[i]++;
-                    if (isGrey(i)) {
-                        tiles[i] = 'G';
-                    } else {
-                        tiles[i] = 'B';
+                while(x-- > 0) {
+                    tiles[cPos] = 'B';
+                    black[cPos]++;
+                    if (x > 0) {
+                        cPos++;
                     }
                 }
             }
-
-            cPos = nPos;
         }
 
         int whiteCount = 0, blackCount = 0, greyCount = 0;
         for(int i = 0; i < tiles.length; i++) {
+            if (isGrey(i)) {
+                greyCount++;
+                continue;
+            }
             if (tiles[i] == 'W') {
                 whiteCount++;
             } else if (tiles[i] == 'B') {
                 blackCount++;
-            } else if (tiles[i] == 'G') {
-                greyCount++;
             }
         }
 

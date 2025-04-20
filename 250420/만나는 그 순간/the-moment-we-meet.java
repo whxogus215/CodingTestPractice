d42ca673455dec44580a62170d8f18f200c95058
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-04-20 14:42:38 +0900

    Title: 만나는 그 순간, Time: 131ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250420/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static final int MAX_LENG = 100000;
+    public static int[] A = new int[MAX_LENG + 1];
+    public static int[] B = new int[MAX_LENG + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        // n번 만큼 A의 좌표를 1초 단위로 기록
+        int time = 1;
+        int signal = 0;
+        for(int i = 0; i < n; i++) {
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+
+            if (d == 'R') {
+                signal = 1;
+            } else {
+                signal = -1;
+            }
+
+            while(t-- > 0) {
+                A[time] = A[time - 1] + signal;
+                time++;
+            }
+        }
+        // m번 만큼 B의 좌표를 1초 단위로 기록
+        time = 1;
+        for(int i = 0; i < m; i++) {
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+
+            if (d == 'R') {
+                signal = 1;
+            } else {
+                signal = -1;
+            }
+
+            while(t-- > 0) {
+                B[time] = B[time - 1] + signal;
+                time++;
+            }
+        }
+
+        int answer = -1;
+        // A, B의 총 이동시간이 같으므로 두 배열을 동시에 확인 O(1000)
+        for(int i = 1; i <= MAX_LENG; i++) {
+            if (A[i] == B[i]) {
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}

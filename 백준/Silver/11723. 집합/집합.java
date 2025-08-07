d4f336e920cae6bb54834778cc59180633d8242c
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-07 15:48:15 +0900

    [Silver V] Title: 집합, Time: 1144 ms, Memory: 300264 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -3,9 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 class Main {
 
@@ -13,39 +10,34 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int M = Integer.parseInt(br.readLine());
+        int S = 0;
 
-        Set<Integer> S = new HashSet<>();
-        List<Integer> allElements = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
         while (M-- > 0) {
             String line = br.readLine();
             if (line.equals("all")) {
-                S = new HashSet<>(allElements);
+                S = 0b00000000000111111111111111111110;
             } else if (line.equals("empty")) {
-                S.clear();
+                S = 0;
             } else {
                 String[] split = line.split(" ");
                 String command = split[0];
                 int x = Integer.parseInt(split[1]);
 
                 if (command.equals("add")) {
-                    S.add(x);
+                    S = S | (1 << x);
                 }
                 if (command.equals("remove")) {
-                    S.remove(x);
+                    S = S & ~(1 << x);
                 }
                 if (command.equals("check")) {
-                    if (S.contains(x)) {
+                    if ((S & (1 << x)) >> x == 1) {
                         bw.write(1 + "\n");
                     } else {
                         bw.write(0 + "\n");
                     }
                 }
                 if (command.equals("toggle")) {
-                    if (S.contains(x)) {
-                        S.remove(x);
-                    } else {
-                        S.add(x);
-                    }
+                    S = S ^ (1 << x);
                 }
             }
         }

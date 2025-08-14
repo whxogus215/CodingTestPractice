a2e4664ea7de4a94db69503d774dc829e9359324
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-14 22:57:47 +0900

    [Silver I] Title: RGB거리, Time: 232 ms, Memory: 20084 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -2,35 +2,32 @@ import java.util.Scanner;
 
 class Main {
 
-    private static final int RED = 0;
-    private static final int GREEN = 1;
-    private static final int BLUE = 2;
-
-    private static int[][] rgb;
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        rgb = new int[N][3];
+        int[] red = new int[N];
+        int[] green = new int[N];
+        int[] blue = new int[N];
+
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                rgb[i][j] = sc.nextInt();
-            }
+            red[i] = sc.nextInt();
+            green[i] = sc.nextInt();
+            blue[i] = sc.nextInt();
         }
 
         int[][] dp = new int[N][3];
-        dp[0][RED] = rgb[0][RED];
-        dp[0][GREEN] = rgb[0][GREEN];
-        dp[0][BLUE] = rgb[0][BLUE];
 
-        for(int i = 1; i < N; i++) {
-            dp[i][RED] = Math.min(dp[i - 1][GREEN], dp[i - 1][BLUE]) + rgb[i][RED];
-            dp[i][GREEN] = Math.min(dp[i - 1][RED], dp[i - 1][BLUE]) + rgb[i][GREEN];
-            dp[i][BLUE] = Math.min(dp[i - 1][RED], dp[i - 1][GREEN]) + rgb[i][BLUE];
+        dp[0][0] = red[0];
+        dp[0][1] = green[0];
+        dp[0][2] = blue[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + red[i];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + green[i];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + blue[i];
         }
 
-        System.out.println(Math.min(Math.min(dp[N - 1][RED], dp[N - 1][GREEN]), dp[N - 1][BLUE]));
+        System.out.println(Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]));
     }
 }

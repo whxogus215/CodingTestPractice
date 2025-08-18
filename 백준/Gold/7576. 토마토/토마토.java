4ee596550dd0c035be839b8ea346dead8e30b3f7
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-18 18:38:43 +0900

    [Gold V] Title: 토마토, Time: 1504 ms, Memory: 337916 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,78 +1,70 @@
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.Scanner;
 
 class Main {
 
-    private static final int DIR = 4;
+    static class Tomato {
+
+        int y;
+        int x;
+        int day;
+
+        public Tomato(int y, int x, int day) {
+            this.y = y;
+            this.x = x;
+            this.day = day;
+        }
+    }
+
     private static final int[] dy = {0, 1, 0, -1};
     private static final int[] dx = {1, 0, -1, 0};
-
-    private static int M, N;
+    private static final int DIR = 4;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        M = sc.nextInt();
-        N = sc.nextInt();
+        int M = sc.nextInt();
+        int N = sc.nextInt();
 
-        int[][] tomato = new int[N][M];
-        int[][] visited = new int[N][M];
+        int[][] tomatos = new int[N][M];
+        Queue<Tomato> queue = new ArrayDeque<>();
 
-        int mustCount = 0;
-        int nowTomato = 0;
-
-        Queue<Node> queue = new ArrayDeque<>();
+        int mustTomato = 0;
+        int currentTomato = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 int value = sc.nextInt();
                 if (value != -1) {
+                    mustTomato++;
                     if (value == 1) {
-                        queue.add(new Node(i, j, 0));
-                        visited[i][j] = 1;
+                        queue.add(new Tomato(i, j, 0));
                     }
-                    mustCount++;
                 }
-                tomato[i][j] = value;
+                tomatos[i][j] = value;
             }
         }
 
+        int minDay = -1;
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            nowTomato++;
-            if (nowTomato == mustCount) {
-                System.out.println(node.time);
-                return ;
+            Tomato tomato = queue.poll();
+            currentTomato++;
+
+            if (currentTomato == mustTomato) {
+                minDay = tomato.day;
+                break;
             }
 
             for (int i = 0; i < DIR; i++) {
-                int ny = node.y + dy[i];
-                int nx = node.x + dx[i];
+                int ny = tomato.y + dy[i];
+                int nx = tomato.x + dx[i];
 
-                if (inRange(ny, nx) && (visited[ny][nx] == 0) && tomato[ny][nx] == 0) {
-                    tomato[ny][nx] = 1;
-                    visited[ny][nx] = 1;
-                    queue.add(new Node(ny, nx, node.time + 1));
+                if (ny >= 0 && ny < N && nx >= 0 && nx < M && tomatos[ny][nx] == 0) {
+                    queue.add(new Tomato(ny, nx, tomato.day + 1));
+                    tomatos[ny][nx] = 1;
                 }
             }
         }
-        System.out.println(-1);
-    }
 
-    private static boolean inRange(int y, int x) {
-        return y >= 0 && y < N && x >= 0 && x < M;
-    }
-
-    static class Node {
-
-        int y;
-        int x;
-        int time;
-
-        public Node(int y, int x, int time) {
-            this.y = y;
-            this.x = x;
-            this.time = time;
-        }
+        System.out.println(minDay);
     }
 }

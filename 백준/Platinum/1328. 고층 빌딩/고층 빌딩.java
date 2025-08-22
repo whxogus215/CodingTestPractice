2ffc2ebfc1f39369b3e8ab64eaeb2b4b6072b38c
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-22 10:04:27 +0900

    [Platinum V] Title: 고층 빌딩, Time: 208 ms, Memory: 26124 KB -BaekjoonHub

---
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    private static final int MOD = 1000000007;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+
+        long[][][] dp = new long[N + 1][L + 1][R + 1];
+        dp[1][1][1] = 1;
+
+        for(int i = 2; i <= N; i++) {
+            for(int j = 1; j <= L; j++) {
+                for(int k = 1; k <= R; k++) {
+                    dp[i][j][k] = (dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] + dp[i - 1][j][k] * (i - 2)) % MOD;
+                }
+            }
+        }
+        
+        System.out.println(dp[N][L][R]);
+    }
+}
+    

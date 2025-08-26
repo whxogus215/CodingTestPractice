52db18e4ecd2cd1780117f64e82f501279b562f6
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-26 22:09:47 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 484 ms, Memory: 61160 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -8,37 +8,23 @@ class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[][] arr = new int[n][m];
+        int[][] dp = new int[n][m];
+        int max = 0;
         for(int i = 0; i < n; i++) {
             String line = sc.next();
             for(int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(String.valueOf(line.charAt(j)));
-            }
-        }
-        
-        int[][] dp = new int[n][m];
-        for(int i = 0; i < m; i++) {
-            dp[0][i] = arr[0][i];
-        }
-        for(int i = 0; i < n; i++) {
-            dp[i][0] = arr[i][0];
-        }
-
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                if (arr[i][j] == 1) {
-                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                int number = Integer.parseInt(String.valueOf(line.charAt(j)));
+                if (i > 0 && j > 0) {
+                    if (number == 1) {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                    }
+                } else {
+                   dp[i][j] = number; 
                 }
-            }
-        }
-
-        int maxLength = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                maxLength = Math.max(maxLength, dp[i][j]);
+                max = Math.max(max, dp[i][j]);
             }
         }
         
-        System.out.println(maxLength * maxLength);
+        System.out.println(max * max);
     }
 }

fa6f66f53877f4925e49d5049df8d270ade390e1
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-08-28 09:17:57 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 456 ms, Memory: 60352 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -14,17 +14,14 @@ class Main {
             String line = sc.next();
             for(int j = 0; j < m; j++) {
                 int number = Integer.parseInt(String.valueOf(line.charAt(j)));
-                if (i > 0 && j > 0) {
-                    if (number == 1) {
-                        dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
-                    }
+                if (i > 0 && j > 0 && number == 1) {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 } else {
-                   dp[i][j] = number; 
+                    dp[i][j] = number;    
                 }
                 max = Math.max(max, dp[i][j]);
             }
         }
-        
         System.out.println(max * max);
     }
 }

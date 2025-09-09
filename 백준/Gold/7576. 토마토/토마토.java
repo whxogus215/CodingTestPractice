50e9756af4ccd355d0c3587bcd10a774c96ed04c
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-09-09 16:20:38 +0900

    [Gold V] Title: 토마토, Time: 1500 ms, Memory: 336988 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,11 +1,60 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Main {
 
-    static class Tomato {
+    private static final int DIR = 4;
+    private static int[] dy = {0, 1, 0, -1};
+    private static int[] dx = {1, 0, -1, 0};
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
 
+        int[][] tomato = new int[N][M];
+        int current = 0;
+        int must = 0;
+        Queue<Tomato> queue = new ArrayDeque<>();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                int value = sc.nextInt();
+                tomato[i][j] = value;
+                if (value != -1) {
+                    must++;
+                    if (value == 1) {
+                        queue.add(new Tomato(i, j, 0));
+                    }
+                }
+            }
+        }
+            
+        int minDay = 0;
+        while(!queue.isEmpty()) {
+            Tomato t = queue.poll();
+            current++;
+            minDay = Math.max(minDay, t.day);
+
+            for(int i = 0; i < DIR; i++) {
+                int ny = t.y + dy[i];
+                int nx = t.x + dx[i];
+
+                if ((ny >= 0 && ny < N && nx >= 0 && nx < M) && tomato[ny][nx] == 0) {
+                    tomato[ny][nx] = 1;
+                    queue.add(new Tomato(ny, nx, t.day + 1));
+                }
+            }
+        }
+
+        if (current < must) {
+            System.out.println(-1);           
+        } else {
+            System.out.println(minDay);
+        }
+    }
+
+    static class Tomato {
         int y;
         int x;
         int day;
@@ -15,56 +64,5 @@ class Main {
             this.x = x;
             this.day = day;
         }
-    }
-
-    private static final int[] dy = {0, 1, 0, -1};
-    private static final int[] dx = {1, 0, -1, 0};
-    private static final int DIR = 4;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-
-        int[][] tomatos = new int[N][M];
-        Queue<Tomato> queue = new ArrayDeque<>();
-
-        int mustTomato = 0;
-        int currentTomato = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                int value = sc.nextInt();
-                if (value != -1) {
-                    mustTomato++;
-                    if (value == 1) {
-                        queue.add(new Tomato(i, j, 0));
-                    }
-                }
-                tomatos[i][j] = value;
-            }
-        }
-
-        int minDay = -1;
-        while (!queue.isEmpty()) {
-            Tomato tomato = queue.poll();
-            currentTomato++;
-
-            if (currentTomato == mustTomato) {
-                minDay = tomato.day;
-                break;
-            }
-
-            for (int i = 0; i < DIR; i++) {
-                int ny = tomato.y + dy[i];
-                int nx = tomato.x + dx[i];
-
-                if (ny >= 0 && ny < N && nx >= 0 && nx < M && tomatos[ny][nx] == 0) {
-                    queue.add(new Tomato(ny, nx, tomato.day + 1));
-                    tomatos[ny][nx] = 1;
-                }
-            }
-        }
-
-        System.out.println(minDay);
     }
 }

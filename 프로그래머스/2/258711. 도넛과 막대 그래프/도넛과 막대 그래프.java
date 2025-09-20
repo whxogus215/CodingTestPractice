5f62748f3b4885f295a561a961ff8ec888bdb417
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-09-20 17:46:33 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 340.19 ms, Memory: 315 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,36 +1,67 @@
 import java.util.*;
 
 class Solution {
+    
+    private static final int MAX = 1000000;
+    
+    boolean[] visited = new boolean[MAX + 1];
+    
     public int[] solution(int[][] edges) {
-        Map<Integer, Integer> in = new HashMap<>();
-        Map<Integer, Integer> out = new HashMap<>();
         int[] answer = new int[4];
-        
-        for(int i = 0; i < edges.length; i++) {
-            in.put(edges[i][1], in.getOrDefault(edges[i][1], 0) + 1);
-            out.put(edges[i][0], out.getOrDefault(edges[i][0], 0) + 1);
+
+        int[] in = new int[MAX + 1];
+        int[] out = new int[MAX + 1];
+        List<Integer>[] edge = new LinkedList[MAX + 1];
+        for(int i = 1; i <= MAX; i++) {
+            edge[i] = new LinkedList<>();
         }
         
-        for(Integer node : out.keySet()) {
-            int count = out.get(node);
-            if (count >= 2) {
-                if (!in.containsKey(node)) {
-                    answer[0] = node;
-                } else {
-                    answer[3]++;
-                }
+        for(int i = 0; i < edges.length; i++) {
+            out[edges[i][0]]++;
+            in[edges[i][1]]++;
+            edge[edges[i][0]].add(edges[i][1]);
+        }
+        
+        for(int i = 1; i <= MAX; i++) {
+            if (in[i] == 0 && out[i] >= 2) {
+                answer[0] = i;
+                break;
             }
         }
         
-        for(Integer node : in.keySet()) {
-            Integer count = in.get(node);
-            if (!out.containsKey(node)) {
+        for(Integer current : edge[answer[0]]) {
+            int count = findCycleCount(current, edge);
+            
+            if (count == 1) {
+                answer[1]++;
+            } else if (count == 2) {
+                answer[3]++;
+            } else {
                 answer[2]++;
             }
         }
         
-        answer[1] = out.get(answer[0]) - (answer[2] + answer[3]);
-        
         return answer;
+    }
+    
+    private int findCycleCount(int start, List<Integer>[] edge) {
+        int cycleCount = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited[start] = true;
+        
+        while(!queue.isEmpty()) {
+            Integer now = queue.poll();
+            for(Integer next : edge[now]) {
+                if (visited[next]) {
+                    cycleCount++;
+                    continue;
+                }
+                queue.add(next);
+                visited[next] = true;
+            }
+        }
+        
+        return cycleCount;     
     }
 }

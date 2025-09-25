d874368d387a6c29f320e6339f62d9c9886ad5dd
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-09-25 13:40:19 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,4 +1,15 @@
 -- 코드를 입력하세요
 SELECT
-COUNT(DISTINCT NAME)
-FROM ANIMAL_INS;
+    COUNT(T.NAME)
+FROM
+    (
+    SELECT
+        NAME
+    FROM
+        ANIMAL_INS
+    WHERE
+        NAME IS NOT NULL
+    GROUP BY
+        NAME
+    ) as T
+;

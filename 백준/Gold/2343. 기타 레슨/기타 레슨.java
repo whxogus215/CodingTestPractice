482a478a7944aa5a680b0248cf3a86ce21d103e9
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-10-21 20:55:07 +0900

    [Gold V] Title: 기타 레슨, Time: 712 ms, Memory: 102544 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.java
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -8,36 +8,41 @@ class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        int[] arr = new int[N];
-        int min = 0;
-        int max = 0;
+        int low = 0;
+        int high = 0;
+        int[] videos = new int[N];
         for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-            min = Math.max(min, arr[i]);
-            max += arr[i];
+            videos[i] = sc.nextInt();
+            low = Math.max(low, videos[i]);
+            high += videos[i];
         }
 
-        while(min <= max) {
-            int mid = min + (max - min) / 2;
+        int result = Integer.MAX_VALUE;
+        while(low <= high) {
+            int mid = low + (high - low) / 2; // 블루레이 크기
+
             int count = 0;
             int sum = 0;
-            for(int i = 0; i < arr.length; i++) {
-                if (sum + arr[i] > mid) {
+            for(int i = 0; i < N; i++) {
+                if (sum + videos[i] > mid) {
                     count++;
                     sum = 0;
                 }
-                sum += arr[i];
+                sum += videos[i];
             }
-            if (sum != 0) {
+
+            if (sum > 0) {
                 count++;
             }
 
             if (count <= M) {
-                max = mid - 1;
+                result = Math.min(result, mid);
+                high = mid - 1;
             } else {
-                min = mid + 1;
+                low = mid + 1;
             }
         }
-        System.out.println(min);
+
+        System.out.println(result);
     }
 }

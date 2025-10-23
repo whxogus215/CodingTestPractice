c279c721b413bdbd99abdb13f03f74a26e37d392
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-10-23 17:42:32 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1128 ms, Memory: 194468 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while(T-- > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            int[] A = new int[N];
+            for(int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+            }
+
+            int[] B = new int[M];
+            for(int i = 0; i < M; i++) {
+                B[i] = sc.nextInt();
+            }
+
+            int count = 0;
+            Arrays.sort(B);
+
+            for(int target : A) {
+                int low = 0;
+                int high = B.length - 1;
+                while(low <= high) {
+                    int mid = low + (high - low) / 2;
+                    if (B[mid] < target) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+                count += low;
+            }
+            System.out.println(count);
+        }
+    }
+}

4e8e00a60735ea8e3fad66f6957157ce46d4ea7c
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-12-15 13:26:52 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 620 ms, Memory: 64292 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
@@ -6,20 +6,22 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+
         double[] arr = new double[N];
         for(int i = 0; i < N; i++) {
             arr[i] = sc.nextDouble();
         }
-        double max = arr[0];
-        double sum = arr[0];
-        
-        for(int i = 1; i < N; i++) {
-            if (arr[i] * sum < arr[i]) {
-                sum = arr[i];
+
+        double cur = arr[0];
+        double max = cur;
+
+        for(int i = 1; i < arr.length; i++) {
+            if (cur > 1) {
+                cur *= arr[i];
             } else {
-                sum *= arr[i];
+                cur = arr[i];
             }
-            max = Math.max(max, sum);
+            max = Math.max(max, cur);
         }
         
         System.out.printf("%.3f", max);

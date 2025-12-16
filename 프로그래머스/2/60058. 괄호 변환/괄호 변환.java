08570bf07300c02ab25c18777cf7290775252bec
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-12-16 17:18:55 +0900

    [level 2] Title: 괄호 변환, Time: 11.35 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,51 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String p) {
-        if (p.isEmpty()) {
-            return p;
-        }
-        
-        int idx = findSplitIdx(p);
-        String u = p.substring(0, idx + 1);
-        String v = p.substring(idx + 1, p.length());
-        
-        if (isCorrect(u)) {
-            return u + solution(v);
-        }        
-        
-        String result = '(' + solution(v) + ')';
-        
-        for(int i = 1; i < u.length() - 1; i++) {
-            if (u.charAt(i) == '(') {
-                result += ')';
-            } else {
-                result += '(';
-            }
-        }
-        return result;
-    }
     
-    private int findSplitIdx(String str) {
+    private int splitIdx = -1;
+    
+    private void doSplit(String str) {
         int open = 0;
         int close = 0;
-        for(int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
+        int idx = 0;
+        for(char c : str.toCharArray()) {
             if (c == '(') {
                 open++;
             } else {
                 close++;
             }
+            idx++;
             if (open == close) {
-                return i;
+                splitIdx = idx;
+                return ;
             }
         }
-        return str.length();
     }
     
     private boolean isCorrect(String str) {
         Deque<Character> stack = new ArrayDeque<>();
-        for(char c : str.toCharArray()) {
+        for(Character c : str.toCharArray()) {
             if (c == '(') {
                 stack.offerFirst(c);
             } else {
@@ -55,10 +34,32 @@ class Solution {
                 stack.pollFirst();
             }
         }
-        
         if (!stack.isEmpty()) {
             return false;
         }
         return true;
+    }
+    
+    public String solution(String p) {
+        if (p.isEmpty()) {
+            return p;
+        }
+        doSplit(p);
+        String u = p.substring(0, splitIdx);
+        String v = p.substring(splitIdx, p.length());
+        
+        if (isCorrect(u)) {
+            return u + solution(v);
+        }
+        
+        String answer = "(" + solution(v) + ")";
+        for(int i = 1; i < u.length() - 1; i++) {
+            if (u.charAt(i) == '(') {
+                answer += ')';
+            } else {
+                answer += '(';
+            }
+        }
+        return answer;
     }
 }

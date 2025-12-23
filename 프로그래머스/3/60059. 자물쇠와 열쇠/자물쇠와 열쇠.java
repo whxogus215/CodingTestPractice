55ed048605509fd1d36a13d4f1001877c6215898
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-12-23 14:31:25 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 52.24 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
-        // 회전 + 이동 -> 표본 수가 작으므로, 완전탐색!
-        int N = lock.length;
-        int M = key.length;
         int offset = key.length - 1;
         for(int r = 0; r < offset + lock.length; r++) {
             for(int c = 0; c < offset + lock.length; c++) {
                 for(int rot = 0; rot < 4; rot++) {
-                    int[][] arr = new int[2 * M + N - 2][2 * M + N - 2];
-                    
-                    for(int i = 0; i < N; i++) {
-                        for(int j = 0; j < N; j++) {
+                    int m = key.length;
+                    int n = lock.length;
+                    int[][] arr = new int[2 * m + n - 2][2 * m + n - 2];
+                    for(int i = 0; i < lock.length; i++) {
+                        for(int j = 0; j < lock.length; j++) {
                             arr[i + offset][j + offset] = lock[i][j];
                         }
                     }
                     
-                    addKey(r, c, rot, arr, key);
+                    simulate(arr, key, r, c, rot);
                     
-                    if (isOpen(arr, lock, offset)) {
+                    if (isCorrect(arr, lock, offset)) {
                         return true;
                     }
                 }
@@ -26,7 +24,7 @@ class Solution {
         return false;
     }
     
-    private void addKey(int r, int c, int rot, int[][] arr, int[][] key) {
+    private void simulate(int[][] arr, int[][] key, int r, int c, int rot) {
         int n = key.length;
         for(int i = 0; i < key.length; i++) {
             for(int j = 0; j < key.length; j++) {
@@ -43,10 +41,10 @@ class Solution {
         }
     }
     
-    private boolean isOpen(int[][] arr, int[][] lock, int offset) {
-        for(int i = 0; i < lock.length; i++) {
-            for(int j = 0; j < lock.length; j++) {
-                if (arr[i + offset][j + offset] != 1) {
+    private boolean isCorrect(int[][] arr, int[][] lock, int offset) {
+        for(int i = offset; i < offset + lock.length; i++) {
+            for(int j = offset; j < offset + lock.length; j++) {
+                if (arr[i][j] != 1) {
                     return false;
                 }
             }
@@ -55,6 +53,5 @@ class Solution {
     }
 }
 
-// 자물쇠 영역 내에서는 열쇠의 돌기 부분과 자물쇠의 홈 부분이 정확히 일치해야 하며
-// 열쇠의 돌기와 자물쇠의 돌기가 만나서는 안됩니다.
-// 자물쇠의 모든 홈을 채워 비어있는 곳이 없어야 자물쇠를 열 수 있습니다.
+// 자물쇠의 홈 + 열쇠의 돌기 O -> lock의 모든 영역이 정확히 1이어야 True
+// 자물쇠의 돌기 + 열쇠의 돌기 X

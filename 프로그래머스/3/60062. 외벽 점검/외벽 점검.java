bf687a290cec4a3ec51664cd04606229fd89db4f
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-12-26 16:56:22 +0900

    [level 3] Title: 외벽 점검, Time: 54.92 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -2,67 +2,66 @@ import java.util.*;
 
 class Solution {
     
-    private static final int MAX = Integer.MAX_VALUE;
-    private int minCount = MAX;
-    private int n;
-    private int[] weak;
+    private final int FRIEND_MAX = 9; 
+    private int needFriendCount = FRIEND_MAX;
+    
     private int[] dist;
+    private int[] weak;
     private int[] weakExpanded;
     private boolean[] visited;
     
     public int solution(int n, int[] weak, int[] dist) {
-        this.n = n;
         this.weak = weak;
         this.dist = dist;
-        this.weakExpanded = new int[weak.length * 2];
-        this.visited = new boolean[dist.length];
         
+        // 원형 배열을 선형 배열로 변환
+        this.weakExpanded = new int[2 * weak.length];
         for(int i = 0; i < weak.length; i++) {
             weakExpanded[i] = weak[i];
             weakExpanded[i + weak.length] = weak[i] + n;
         }
         
-        dfs(0, new int[dist.length]);
+        // 순열 -> 각 순열마다 완전탐색하기
+        this.visited = new boolean[dist.length];
+        dfs(new int[dist.length], 0);
         
-        if (minCount == MAX) {
+        if (needFriendCount == FRIEND_MAX) {
             return -1;
         }
-        return minCount;
+        return needFriendCount;
     }
     
-    private void dfs(int depth, int[] result) {
+    private void dfs(int[] result, int depth) {
         if (depth == dist.length) {
-            simulate(result);            
-            return ; 
+            simulate(result);
+            return ;
         }
-        
         for(int i = 0; i < dist.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                result[depth] = dist[i];
-                dfs(depth + 1, result);
-                visited[i] = false;
+            if (visited[i]) {
+                continue;
             }
+            result[depth] = dist[i];
+            visited[i] = true;
+            dfs(result, depth + 1);
+            visited[i] = false;
         }
     }
     
     private void simulate(int[] friendOrder) {
         for(int start = 0; start < weak.length; start++) {
             int cursor = start;
-            int searchCount = 0;
+            int friendCount = 0;
             for(int i = 0; i < friendOrder.length; i++) {
                 int end = weakExpanded[cursor] + friendOrder[i];
-                while (cursor < weakExpanded.length && weakExpanded[cursor] <= end) {
-                    searchCount++;
+                while(cursor < weakExpanded.length && weakExpanded[cursor] <= end) {
                     cursor++;
                 }
-                if (searchCount >= weak.length) {
-                    minCount = Math.min(minCount, (i + 1));
+                friendCount++;
+                if (cursor - start >= weak.length) {
+                    needFriendCount = Math.min(needFriendCount, friendCount);
                     break;
                 }
             }
         }
     }
 }
-
-// 각 순열에 대해 취약점 각 부분을 시작점으로 하여 시뮬레이션 -> 최소 값 갱신

2232141231af24c4270248a2fc9e3015a7c08d54
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2025-12-31 14:27:49 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 57.75 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,21 +1,23 @@
 class Solution {
+    
+    private int N, M;
+    
     public boolean solution(int[][] key, int[][] lock) {
-        int offset = key.length - 1;
-        for(int r = 0; r < offset + lock.length; r++) {
-            for(int c = 0; c < offset + lock.length; c++) {
+        N = lock.length;
+        M = key.length;
+        
+        for(int r = 0; r < N + M - 1; r++) {
+            for(int c = 0; c < N + M - 1; c++) {
                 for(int rot = 0; rot < 4; rot++) {
-                    int m = key.length;
-                    int n = lock.length;
-                    int[][] arr = new int[2 * m + n - 2][2 * m + n - 2];
+                    int[][] arr = new int[2 * M + N - 2][2 * M + N - 2];
                     for(int i = 0; i < lock.length; i++) {
                         for(int j = 0; j < lock.length; j++) {
-                            arr[i + offset][j + offset] = lock[i][j];
+                            arr[i + M - 1][j + M - 1] = lock[i][j];
                         }
                     }
-                    
-                    simulate(arr, key, r, c, rot);
-                    
-                    if (isCorrect(arr, lock, offset)) {
+                    simulate(r, c, rot, arr, key);
+
+                    if (isCorrect(arr, lock)) {
                         return true;
                     }
                 }
@@ -24,27 +26,26 @@ class Solution {
         return false;
     }
     
-    private void simulate(int[][] arr, int[][] key, int r, int c, int rot) {
-        int n = key.length;
+    private void simulate(int r, int c, int rot, int[][] arr, int[][] key) {
         for(int i = 0; i < key.length; i++) {
             for(int j = 0; j < key.length; j++) {
                 if (rot == 0) {
-                    arr[i + r][j + c] += key[i][j];
-                } else if (rot == 1) {
-                    arr[i + r][j + c] += key[j][n - 1 - i];
-                } else if (rot == 2) {
-                    arr[i + r][j + c] += key[n - 1 - i][n - 1 - j];
-                } else {
-                    arr[i + r][j + c] += key[n - 1 - j][i];
+                    arr[r + i][c + j] += key[i][j];
+                } else if (rot == 1) { // 0, 1 -> 1, 2 // 1, 2 -> 2, 1
+                    arr[r + i][c + j] += key[j][M - 1 - i];
+                } else if (rot == 2) { // 0, 1 -> 2, 1 // 1, 2 -> 1, 0
+                    arr[r + i][c + j] += key[M - 1 - i][M - 1 - j];
+                } else { // 0, 1 -> 1, 0
+                    arr[r + i][c + j] += key[M - 1 - j][i];
                 }
             }
         }
     }
     
-    private boolean isCorrect(int[][] arr, int[][] lock, int offset) {
-        for(int i = offset; i < offset + lock.length; i++) {
-            for(int j = offset; j < offset + lock.length; j++) {
-                if (arr[i][j] != 1) {
+    private boolean isCorrect(int[][] arr, int[][] lock) {
+        for(int i = 0; i < lock.length; i++) {
+            for(int j = 0; j < lock.length; j++) {
+                if (arr[i + M - 1][j + M - 1] != 1) {
                     return false;
                 }
             }
@@ -53,5 +54,4 @@ class Solution {
     }
 }
 
-// 자물쇠의 홈 + 열쇠의 돌기 O -> lock의 모든 영역이 정확히 1이어야 True
-// 자물쇠의 돌기 + 열쇠의 돌기 X
+// 키에 대한 회전 + 이동 조합으로 자물쇠가 열리는지 판단하는 완탐

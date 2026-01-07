e9752c018fbfc2a84014cd391fbd23a73afbcb76
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2026-01-07 09:32:12 +0900

    [level 3] Title: 기지국 설치, Time: 0.62 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,27 +1,33 @@
 class Solution {
+    
+    private int W;
+    
     public int solution(int n, int[] stations, int w) {
+        W = 2 * w + 1;
+        int count = 0;
         int cursor = 1;
-        int needCount = 0;
         for(int i = 0; i < stations.length; i++) {
             int left = stations[i] - w;
             int right = stations[i] + w;
-            if (left > cursor) {
-                int width = left - cursor;
-                needCount += width / (2 * w + 1);
-                if (width % (2 * w + 1) != 0) {
-                    needCount += 1;
-                }
+            if (cursor >= left) {
+                cursor = right + 1;
+                continue;
+            }
+            int width = left - cursor;
+            count += (width / W);
+            if (width % W != 0) {
+                count += 1;
             }
             cursor = right + 1;
         }
-        
         if (cursor <= n) {
-            needCount += (n - cursor + 1) / (2 * w + 1);
-            if ((n - cursor + 1) % (2 * w + 1) != 0) {
-                needCount++;
-            }
+           int width = n - cursor + 1;
+            count += (width / W);
+            if (width % W != 0) {
+                count += 1;
+            } 
         }
-        
-        return needCount;
+
+        return count;
     }
 }
